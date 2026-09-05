a21810fa2cd2ec93ae05f6b65b62cda1e2ae210d
Aayush Singh Chauhan <1211aayushh@gmail.com>
2026-09-05 21:02:36 +0530

    updated fruits animation

--- index.js
+++ index.js
@@ -18,73 +18,56 @@ window.addEventListener('resize', resizeCanvas);
 
 
 
-const fruits = document.querySelectorAll(".fruitImg");
-console.log(fruits.length);
+const fruitsImg = Array.from(document.querySelectorAll(".fruitImg"));
+console.log(fruitsImg.length + " hello ji") ;
 
-const fruitsarray = [];
-
-fruits.forEach((fruit) => {
-    const fruitobject = {
-        image: fruit,
-        w: 100,
-        h: 100,
-        x: Math.floor(Math.random() * (canvas.width - (30 * 2)) + 30),
-        y: canvas.height - 100,
-        dx: Math.floor(Math.random()*5),
-        dy: -Math.floor(Math.random()*5) - 7
-        // dy: -12
-    }
-    fruitsarray.push(fruitobject);
-})
-
-const activeFruits = [];
-console.log(Math.floor(Math.random() * canvas.width - 10));
+let activeFruits = [];
 
 let randomfruit = 1;
-let fruit = fruitsarray[randomfruit]
-const spawnFruit = () => {
-     randomfruit = Math.floor(Math.random()*7);
-     fruit = fruitsarray[randomfruit];
-     fruit.x = Math.random() * (canvas.width - fruit.w);
-    fruit.y = canvas.height - fruit.h;
 
-    fruit.dx = (Math.random() * 4) - 2;
-    fruit.dy = -12;
-    activeFruits.push(fruit);
+const spawnFruit = () => {
+    randomfruit = Math.floor(Math.random() * 7);
+    console.log(randomfruit);
+
+    const newFruit = {
+        image: fruitsImg[randomfruit],
+        w:100,
+        h:100,
+        x:Math.floor(Math.random() * canvas.width - (30*2)+30),
+        y:canvas.height - 100,
+        dx:Math.floor(Math.random() * 5) - 2,
+        dy: -Math.floor(Math.random()*5 + 12),
+    }
+    activeFruits.push(newFruit);
+    console.log("HELLO WORLD");
+
 }
 
-// function drawFruits(fruit) {
-//     console.log(fruit);
-//         ctx.drawImage(fruit.image, fruit.x, fruit.y, fruit.w, fruit.h);
-// }
 
-// drawFruits();
 
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    fruit.x += fruit.dx;
-    fruit.y += fruit.dy;
-
-    if(fruit.x + fruit.w > canvas.width){
-        fruit.dx*=-1;
-    }
-
-    if(fruit.y - fruit.w < 50){
-        fruit.dy *=-1;
-    }
-    
-    fruit.dy+=0.1;
-    
-    activeFruits.forEach((fruit)=>{
+    activeFruits.forEach((fruit) => {
+        fruit.dy +=0.2;
+        fruit.x += fruit.dx;
+        fruit.y += fruit.dy;
+        
         ctx.drawImage(fruit.image, fruit.x, fruit.y, fruit.w, fruit.h);
+        if (fruit.x + fruit.w > canvas.width || fruit.x - fruit.w < 0) {
+            fruit.dx *= -1;
+        }
     })
+
+    activeFruits = activeFruits.filter((fruit)=> fruit.y < canvas.height + 150);
 
     requestAnimationFrame(update);
 
 }
+
+
 spawnFruit();
-spawnFruit();
+setInterval(spawnFruit, 1000);
 update();
 
 
